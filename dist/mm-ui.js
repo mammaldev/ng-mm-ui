a4dd69afd4c88b91f78461978ccbea2aef648407
Ollie Relph <ollie@relph.me>
2014-11-17 12:05:42 +0000

    0.2.0

--- dist/mm-ui.js
+++ dist/mm-ui.js
@@ -49,3 +49,41 @@ angular.module('mm.ui')
     }
   };
 });
+
+angular.module('mm.ui')
+.directive('mmRequireSome', function mmRequireSome() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function ( scope, elem, attr, ngModel ) {
+
+      console.log('mm-r-s');
+
+      scope.$watch(function () {
+        return ngModel.$modelValue;
+      }, function ( newValue ) {
+
+      console.log('mm-r-s', newValue);
+        ngModel.$setViewValue(newValue);
+        ngModel.$render();
+      }, true);
+
+      ngModel.$formatters.unshift(function (value) {
+        ngModel.$setValidity('mmRequireSome', value && value.length > 0);
+        return value;
+      });
+
+      ngModel.$parsers.unshift(function (value) {
+        if (!Array.isArray(value)) {
+          if ( value && value.length > 0) {
+            value = value.split(/[ ,]+/);
+          } else {
+            value = [];
+          }
+        }
+        ngModel.$setValidity('mmRequireSome', value && value.length > 0);
+        return value;
+      });
+    }
+  };
+});
