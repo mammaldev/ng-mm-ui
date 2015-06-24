7aef02248682e9543c37540a2ecf975041d3d8bb
Mike Harris <mike.mj.harris@gmail.com>
2015-06-24 12:49:50 +0100

    add an optional confirm message

--- src/factories/unsaved-work.js
+++ src/factories/unsaved-work.js
@@ -1,13 +1,14 @@
 angular.module('mm.ui')
 .factory('mmUnsavedWork', function ( $rootScope, $window ) {
   var statuses = {};
+  var confirmMessage = 'Your unsaved work will be lost! To continue without saving click OK. To go back and save click Cancel.';
 
   $rootScope.$on('$locationChangeStart', function routeChange( e ) {
     var anyUnsavedWork = Object.keys(statuses).some(function ( key ) {
       return statuses[ key ];
     });
     if ( anyUnsavedWork ) {
-      var continueToNextLink = $window.confirm('Your unsaved work will be lost! To continue without saving click OK. To go back and save click Cancel.');
+      var continueToNextLink = $window.confirm(confirmMessage);
       if ( continueToNextLink ) {
         statuses = {};
       } else {
@@ -17,7 +18,10 @@ angular.module('mm.ui')
   });
 
   return {
-    setStatus: function ( key, value ) {
+    setStatus: function ( key, value, optionalConfirmMessage ) {
+      if ( optionalConfirmMessage ) {
+        confirmMessage = optionalConfirmMessage;
+      }
       statuses[ key ] = value;
     }
   };
