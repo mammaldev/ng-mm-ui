9265780a19c7e825040ee08c0dd96f92629e1024
Nick Cartwright <nick@aslant.io>
2018-09-04 14:24:10 +0200

    Get rid of testing / jshint related code and deps

--- test/module.spec.js
+++
@@ -1,8 +0,0 @@
-describe('mmUi module', function () {
-
-  beforeEach(angular.mock.module('mm.ui'));
-
-  it('should be available', function () {
-    return expect(angular.module('mm.ui')).to.exist;
-  });
-});
